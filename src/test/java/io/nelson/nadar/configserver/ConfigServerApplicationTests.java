2d56b6e3a61f8125f639ddd0d26d2654ab79faad
Nelson Nadar <nelsonnadar@nelsons-mbp.home>
2020-10-28 11:00:20 +0000

    code for config server

---
+++ src/test/java/io/nelson/nadar/configserver/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package io.nelson.nadar.configserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
